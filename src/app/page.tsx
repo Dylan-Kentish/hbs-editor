adf2833bc42247f3064c86c39637905692f034a7
Kent <dylankentish@live.co.uk>
2023-07-22 20:28:02 +0100

    more complex example with inline css and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,134 @@ import Handlebars from 'handlebars';
 import { useState } from 'react';
 
 const defaultTemplate = `
-  <h1>{{title}}</h1>
-  <ul>
-    {{#each items}}
-      <li>{{this}}</li>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Order Confirmation</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      line-height: 1.6;
+    }
+    h1, h2 {
+      color: #555;
+    }
+    .product-card {
+      display: flex;
+      border: 1px solid #ccc;
+      margin-bottom: 20px;
+      padding: 10px;
+    }
+    .product-image {
+      width: 100px;
+      height: 100px;
+      margin-right: 10px;
+    }
+    .product-details {
+      flex: 1;
+    }
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin-bottom: 20px;
+    }
+    th, td {
+      padding: 8px;
+      text-align: left;
+      border-bottom: 1px solid #ccc;
+    }
+    th {
+      background-color: #f2f2f2;
+    }
+    p {
+      margin-bottom: 10px;
+    }
+  </style>
+</head>
+<body>
+  <h1>Order Confirmation</h1>
+  
+  <h2>Order Summary</h2>
+  {{#each products}}
+  <div class="product-card">
+    <div class="product-image">
+      <img src="{{this.image}}" alt="{{this.name}}">
+    </div>
+    <div class="product-details">
+      <p><strong>{{this.name}}</strong></p>
+      <p>Quantity: {{this.quantity}}</p>
+      <p>Price: {{this.price}}</p>
+    </div>
+  </div>
+  {{/each}}
 
-      {{#if @last}}
-        <li>last item</li>
-      {{/if}}
-    {{/each}}
-  </ul>
+  <h2>Shipping Address</h2>
+  <p>
+    {{shippingAddress.name}}
+    <br>
+    {{shippingAddress.address}}
+    <br>
+    {{shippingAddress.city}}, {{shippingAddress.state}} {{shippingAddress.zip}}
+    <br>
+    {{shippingAddress.country}}
+  </p>
+
+  <h2>Payment Method</h2>
+  <p>
+    Payment Method: {{paymentMethod.type}}
+    <br>
+    Cardholder Name: {{paymentMethod.cardholderName}}
+    <br>
+    Card Number: **** **** **** {{paymentMethod.last4}}
+    <br>
+    Expiry Date: {{paymentMethod.expiryDate}}
+  </p>
+
+  <p>
+    Thank you for your order! If you have any questions or concerns, please don't hesitate to contact our customer support.
+  </p>
+</body>
+</html>
+
 `
 
 const defaultData = `
-  {
-    "title": "My new post",
-    "items": [
-      "foo",
-      "bar",
-      "baz"
-    ]
+{
+  "products": [
+    {
+      "name": "Product A",
+      "quantity": 2,
+      "price": "$25.99",
+      "image": "https://via.placeholder.com/150"
+    },
+    {
+      "name": "Product B",
+      "quantity": 1,
+      "price": "$19.95",
+      "image": "https://via.placeholder.com/150"
+    },
+    {
+      "name": "Product C",
+      "quantity": 3,
+      "price": "$12.49",
+      "image": "https://via.placeholder.com/150"
+    }
+  ],
+  "shippingAddress": {
+    "name": "John Doe",
+    "address": "123 Main St",
+    "city": "Anytown",
+    "state": "CA",
+    "zip": "12345",
+    "country": "United States"
+  },
+  "paymentMethod": {
+    "type": "Credit Card",
+    "cardholderName": "John Doe",
+    "last4": "7890",
+    "expiryDate": "09/25"
   }
+}
 `
 
 function update(template: string, data: string) {
